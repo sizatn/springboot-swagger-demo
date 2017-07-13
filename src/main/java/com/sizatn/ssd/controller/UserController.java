e501587cae426bed1f664470eb5b150c2dd08eaf
sizatn <sizatn2013@gmail.com>
2017-07-13 16:11:01 +0800

    update

--- src/main/java/com/sizatn/ssd/controller/UserController.java
+++ src/main/java/com/sizatn/ssd/controller/UserController.java
@@ -4,10 +4,13 @@ import java.util.List;
 
 import javax.annotation.Resource;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.sizatn.ssd.entity.User;
@@ -26,14 +29,14 @@ public class UserController {
 	private UserService userService;
 
     @ApiOperation(value="获取用户列表", notes="")
-    @RequestMapping(value={""}, method=RequestMethod.GET)
+	@GetMapping(value = { "" })
     public List<User> getUserList() {
         return userService.getUserList();
     }
 
     @ApiOperation(value="创建用户", notes="根据User对象创建用户")
     @ApiImplicitParam(name = "user", value = "用户详细实体user", required = true, dataType = "User")
-    @RequestMapping(value="", method=RequestMethod.POST)
+	@PostMapping(value = "")
     public String postUser(@RequestBody User user) {
         if (userService.saveUser(user) != 1) {
         	return "fail";
@@ -43,14 +46,14 @@ public class UserController {
 
     @ApiOperation(value="获取用户详细信息", notes="根据url的id来获取用户详细信息")
     @ApiImplicitParam(name = "userNo", value = "用户编号", required = true, dataType = "String")
-    @RequestMapping(value="/{userNo}", method=RequestMethod.GET)
+	@GetMapping(value = "/{userNo}")
     public User getUser(@PathVariable String userNo) {
         return userService.getUser(userNo);
     }
 
     @ApiOperation(value="更新用户详细信息", notes="根据url的id来指定更新对象，并根据传过来的user信息来更新用户详细信息")
     @ApiImplicitParam(name = "user", value = "用户详细实体user", required = true, dataType = "User")
-    @RequestMapping(value="", method=RequestMethod.PUT)
+	@PutMapping(value = "")
     public String putUser(@RequestBody User user) {
         if (userService.updateUser(user) != 1) {
         	return "fail";
@@ -60,7 +63,7 @@ public class UserController {
 
     @ApiOperation(value="删除用户", notes="根据url的id来指定删除对象")
     @ApiImplicitParam(name = "userNo", value = "用户编号", required = true, dataType = "String")
-    @RequestMapping(value="/{userNo}", method=RequestMethod.DELETE)
+	@DeleteMapping(value = "/{userNo}")
     public String deleteUser(@PathVariable String userNo) {
         if (userService.deleteUser(userNo) != 1) {
         	return "fail";
