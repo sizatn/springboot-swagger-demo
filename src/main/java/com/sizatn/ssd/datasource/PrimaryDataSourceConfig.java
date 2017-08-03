969df3b2466b25bdc5ce6a410644ee1e6b855b49
sizatn <sizatn2013@gmail.com>
2017-08-04 00:01:40 +0800

    add common mapper

--- src/main/java/com/sizatn/ssd/datasource/PrimaryDataSourceConfig.java
+++ src/main/java/com/sizatn/ssd/datasource/PrimaryDataSourceConfig.java
@@ -1,13 +1,10 @@
 package com.sizatn.ssd.datasource;
 
-import java.util.Properties;
-
 import javax.sql.DataSource;
 
-import org.apache.ibatis.plugin.Interceptor;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
-import org.mybatis.spring.mapper.MapperScannerConfigurer;
+import tk.mybatis.spring.mapper.MapperScannerConfigurer;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
 import org.springframework.boot.context.properties.ConfigurationProperties;
@@ -16,8 +13,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
-
-import com.github.pagehelper.PageInterceptor;
 
 @Configuration
 public class PrimaryDataSourceConfig {
