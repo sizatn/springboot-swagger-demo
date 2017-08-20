be20a37b912e720bc49df2e09b77e19b8029713e
sizatn <sizatn2013@gmail.com>
2017-08-20 16:22:45 +0800

    update

---
+++ src/test/java/com/sizatn/ssd/test/AppTest.java
@@ -0,0 +1,14 @@
+package com.sizatn.ssd.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AppTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
