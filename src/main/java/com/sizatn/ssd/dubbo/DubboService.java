40a4eb2aedbf98bbecce27075abe78ef3d407399
sizatn <sizatn2013@gmail.com>
2017-11-28 14:47:21 +0800

    add dubbo

---
+++ src/main/java/com/sizatn/ssd/dubbo/DubboService.java
@@ -0,0 +1,7 @@
+package com.sizatn.ssd.dubbo;
+
+public interface DubboService {
+	
+	String getName();
+
+}
---
+++ src/main/java/com/sizatn/ssd/dubbo/impl/DubboServiceImpl.java
@@ -0,0 +1,14 @@
+package com.sizatn.ssd.dubbo.impl;
+
+import com.alibaba.dubbo.config.annotation.Service;
+import com.sizatn.ssd.dubbo.DubboService;
+
+@Service(group="dubboService", version = "1.0.0")
+public class DubboServiceImpl implements DubboService {
+
+	@Override
+	public String getName() {
+		return "Hello World!";
+	}
+
+}
